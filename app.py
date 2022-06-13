49c6610a2a1fc1b6bc2aa17bb6cbeb9e195c9179
Manideep <72013098+Manideepc007@users.noreply.github.com>
2022-06-13 18:28:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ def index():
 if __name__ == "__main__":
     app.run(debug = True)
 
-# heroku api key - df950c89-2375-4ba3-a37b-0d8e53deb41f
-# heroku app name - fsds-ml-regression
-# heroku email - manimanideep345@gmail.com
+
 
 
